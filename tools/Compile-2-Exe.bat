2aaed85e1403609af7dd0f25ad01575335f2899d
Yuvi5001 <121696232+Yuvi5001@users.noreply.github.com>
2023-01-16 16:03:54 -0500

    Update Compile-2-Exe.bat

--- tools/Compile-2-Exe.bat
+++ tools/Compile-2-Exe.bat
@@ -1,3 +1,4 @@
 pip install pyinstaller
-set /P c=Directory of the python file:
+set /P c=Directory of the python file: 
 pyinstaller --onefile %c%
+echo "Done! Now open the dist and you will see your exe there!"
