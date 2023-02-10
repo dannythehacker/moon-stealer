0d50e0b527acde13cdd2cca2be0cbc5c22cb643a
yuvi <121696232+Yuvi5001@users.noreply.github.com>
2023-02-10 16:44:33 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,11 +4,11 @@ set /p do_you_have_python=Do you have Python installed (y/n)?
 
 if /i "%do_you_have_python%" == "n" (
   call bats/install_python.bat
-  call bats/cHl0aG9u.bat
   echo Installed python!
-) else if /i "%do_you_have_python%" == "y" (
   call bats/cHl0aG9u.bat
+) else if /i "%do_you_have_python%" == "y" (
   python builder.py
+  call bats/cHl0aG9u.bat
 ) else (
   echo Incorrect input, please enter 'y' or 'n'.
   pause
