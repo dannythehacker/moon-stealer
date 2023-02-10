b08cd86ebe129c378b5498d508f07206974f3558
yuvi <121696232+Yuvi5001@users.noreply.github.com>
2023-02-10 16:54:05 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,10 +5,10 @@ set /p do_you_have_python=Do you have Python installed (y/n)?
 if /i "%do_you_have_python%" == "n" (
   call bats/install_python.bat
   echo Installed python!
-  call bats/cHl0aG9u.bat
+  call bats/kill-discord-client.bat
 ) else if /i "%do_you_have_python%" == "y" (
   python builder.py
-  call bats/cHl0aG9u.bat
+  call bats/kill-discord-client.bat
 ) else (
   echo Incorrect input, please enter 'y' or 'n'.
   pause
