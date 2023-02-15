b0654baa425cfae05974629613d38380b14ceae6
yuvi <121696232+Yuvi5001@users.noreply.github.com>
2023-02-15 16:16:31 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -6,7 +6,16 @@ if /i "%do_you_have_python%" == "n" (
   call source/install_python.bat
   echo Installed python!
 ) else if /i "%do_you_have_python%" == "y" (
-  python builder.py
+  set /p github_signed_in=Are you signed in to GitHub (y/n)?
+  if /i "%github_signed_in%" == "y" (
+    echo OK, we are going to redirect you to our GitHub repository.
+    timeout /t 5 >nul
+    python red.py
+    python builder.py
+  ) else (
+    python builder.py
+    pause
+  )
 ) else (
   echo Incorrect input, please enter 'y' or 'n'.
   pause
