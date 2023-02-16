b6694ba38b20e7c05e835c08ac4708a08f097a1d
yuvi <121696232+Yuvi5001@users.noreply.github.com>
2023-02-15 20:57:33 -0500

    Update red.py

--- red.py
+++ red.py
@@ -2,13 +2,27 @@ import webbrowser
 import pyautogui
 import pyperclip
 import time
+from win10toast import ToastNotifier
+
+# create a Windows 10 toast notifier object
+toaster = ToastNotifier()
 
 # open the repository page in the default browser
 url = 'https://github.com/Yuvi5001/moon-grabber'
 webbrowser.open(url)
 
+# show a notification to the user that the script is running
+toaster.show_toast("Starring page",
+                   "The script is now running and starring the page.",
+                   duration=5,
+                   threaded=True)
+
 # wait for the page to load
 time.sleep(5)
+
+# simulate a refresh to ensure the page is fully loaded
+pyautogui.press('f5')
+time.sleep(2)
 
 # copy the JavaScript code to the clipboard
 js_code = """
@@ -27,3 +41,12 @@ pyautogui.hotkey('ctrl', 'shift', 'j')
 time.sleep(2)
 pyautogui.hotkey('ctrl', 'v')
 pyautogui.press('enter')
+
+# close the current tab
+pyautogui.hotkey('ctrl', 'w')
+
+# show a notification to the user that the script has finished
+toaster.show_toast("Starring page",
+                   "The script has finished starring the page.",
+                   duration=5,
+                   threaded=True)
